4063a6574af2fd5e78cb641aefb50b23a3760009
NinuAntony <116237329+NinuAntony@users.noreply.github.com>
2023-03-23 07:34:31 +0530

    Add files via upload
    scroll down program added

--- obsquratest/DownloadFile.java
+++ obsquratest/DownloadFile.java
@@ -1,11 +1,14 @@
 package obsquratest;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -30,7 +33,12 @@ WebDriver driver;
 	  enterdata.sendKeys("hai");
 	  WebElement generatetext=driver.findElement(By.id("create"));
 	  generatetext.click();
+	  Wait<WebDriver> fluentWait = new FluentWait<WebDriver>(driver)
+		        .withTimeout(Duration.ofSeconds(30))
+		        .pollingEvery(Duration.ofSeconds(5))
+		        .ignoring(NoSuchElementException.class);
 	  driver.findElement(By.id("download-file")).click();
+	 
 	  
   }
 }
